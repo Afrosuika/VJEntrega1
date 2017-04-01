5081af7a5ac6654a4b76f4a5d9e82570a8077ccc
OriolCapo <oriolcapo@gmail.com>
2017-04-01 15:49:04 +0200

    Afegit el tercer nivell (despres de passarte el segon)

---
+++ 2DGame/02-Bubble/02-Bubble/Scene3.h
@@ -0,0 +1,56 @@
+#ifndef _SCENE3_INCLUDE
+#define _SCENE3_INCLUDE
+
+
+#include <glm/glm.hpp>
+#include "ShaderProgram.h"
+#include "TileMap.h"
+#include "Player.h"
+#include "Spikes.h"
+#include "Soldier.h"
+#include "Guillotina.h"
+#include "Portagran.h"
+#include "SoundManager2.h"
+#include "Life.h"
+
+// Scene contains all the entities of our game.
+// It is responsible for updating and render them.
+
+
+class Scene3
+{
+
+public:
+	Scene3();
+	~Scene3();
+
+	void init();
+	void update(int deltaTime);
+	void render();
+	bool levelFinished(){ return isLevelFinished; };
+	void stop();
+
+private:
+	void initShaders();
+	glm::vec4 projectionMargins();
+
+private:
+	TileMap *map, *map2;
+	Player *player;
+	Life *life;
+	vector<Spikes*> spikes;
+	vector<Soldier*> soldiers;
+	vector<Guillotina*> guillotines;
+	Portagran* portagran;
+	ShaderProgram texProgram;
+	SoundManager2 *manager;
+	float currentTime;
+	glm::mat4 projection;
+	float marginLeft, marginTop;
+	bool isLevelFinished;
+	bool dontRender;
+};
+
+
+#endif // _SCENE3_INCLUDE
+
